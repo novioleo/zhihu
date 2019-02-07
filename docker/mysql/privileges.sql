b82e5bea6083d728a409a6064dc4c2a3d09f4f45
novioleo <nsnovio@gmail.com>
2019-02-07 10:54:51 +0800

    将初始化的sql放置在mysql_init中

--- docker/mysql/privileges.sql
+++
@@ -1,4 +0,0 @@
-use mysql
-create user docker identified by '123456';
-grant all on zhihu.* to docker@'%' identified by '123456' with grant option;
-flush privileges;
--- docker/mysql/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-service mysql start
-mysql -uroot -p123456 < /mysql/zhihu.sql
-mysql -uroot -p123456 < /mysql/privileges.sql
-tail -f /dev/null
